3f184c3c33a710d39d3e9127da5bba2248c942a5
pdqnguyen <philnguyen94@gmail.com>
2021-10-22 15:12:16 -0700

    add more plots, subsections for noise studies

--- scripts/site-wide.py
+++ scripts/site-wide.py
@@ -36,43 +36,43 @@ ambient_dict = dict(
 
 colors = ['#015d8e', '#85c0f9', '#f5793a']
 
-# # figure for each coupling type, subplot for each observatory
-# markersizes = dict(
-#     vib=2,
-#     mag=5,
-# )
-# for coupling_type in ambient_dict.keys():
-#     fig, ax = plt.subplots(2, 1, figsize=(5, 4))
-#     plt.subplots_adjust(left=0.2, bottom=0.12, right=0.99, top=0.95, hspace=0.2)
-#     path = os.path.join(OUT_DIR, f"ambient_{coupling_type}.pdf")
-#     for i, (observatory, df) in enumerate(ambient_dict[coupling_type].items()):
-#         darm = darm_dict[observatory]
-#         ax[i].loglog(darm.frequency, darm.darm, 'k-')
-#         ax[i].plot(
-#             df.frequency[df.flag == 'Measured'],
-#             df.amb[df.flag == 'Measured'],
-#             '.',
-#             color=colors[0],
-#             markersize=markersizes[coupling_type],
-#             zorder=3
-#         )
-#         ax[i].plot(
-#             df.frequency[df.flag == 'Upper Limit'],
-#             df.amb[df.flag == 'Upper Limit'],
-#             '.',
-#             color=colors[1],
-#             markersize=markersizes[coupling_type],
-#             zorder=2
-#         )
-#         ax[i].plot(aligo_sens[0], 4000 * np.sqrt(aligo_sens[1]), lw=3, color=colors[2], zorder=1)
-#         ax[i].set_xlim(5, 2048)
-#         ax[i].set_ylim(1e-23, 1e-16)
-#         ax[i].grid(which='major', axis='both', lw=1)
-#         ax[i].grid(which='minor', axis='both', lw=0.7, ls='--')
-#         ax[i].set_axisbelow(True)
-#         ax[i].set_ylabel(observatory.upper() + r' DARM $\left[\mathrm{m}/\mathrm{Hz}^{1/2}\right]$')
-#     ax[-1].set_xlabel('Frequency [Hz]')
-#     plt.savefig(path)
+# figure for each coupling type, subplot for each observatory
+markersizes = dict(
+    vib=2,
+    mag=5,
+)
+for coupling_type in ambient_dict.keys():
+    fig, ax = plt.subplots(2, 1, figsize=(5, 4))
+    plt.subplots_adjust(left=0.2, bottom=0.12, right=0.99, top=0.95, hspace=0.2)
+    path = os.path.join(OUT_DIR, f"ambient_{coupling_type}.pdf")
+    for i, (observatory, df) in enumerate(ambient_dict[coupling_type].items()):
+        darm = darm_dict[observatory]
+        ax[i].loglog(darm.frequency, darm.darm, 'k-')
+        ax[i].plot(
+            df.frequency[df.flag == 'Measured'],
+            df.amb[df.flag == 'Measured'],
+            '.',
+            color=colors[0],
+            markersize=markersizes[coupling_type],
+            zorder=3
+        )
+        ax[i].plot(
+            df.frequency[df.flag == 'Upper Limit'],
+            df.amb[df.flag == 'Upper Limit'],
+            '.',
+            color=colors[1],
+            markersize=markersizes[coupling_type],
+            zorder=2
+        )
+        ax[i].plot(aligo_sens[0], 4000 * np.sqrt(aligo_sens[1]), lw=3, color=colors[2], zorder=1)
+        ax[i].set_xlim(5, 2048)
+        ax[i].set_ylim(1e-23, 1e-16)
+        ax[i].grid(which='major', axis='both', lw=1)
+        ax[i].grid(which='minor', axis='both', lw=0.7, ls='--')
+        ax[i].set_axisbelow(True)
+        ax[i].set_ylabel(observatory.upper() + r' DARM $\left[\mathrm{m}/\mathrm{Hz}^{1/2}\right]$')
+    ax[-1].set_xlabel('Frequency [Hz]')
+    plt.savefig(path)
 
 # all-in-one-figure plot
 coupling_names = dict(
